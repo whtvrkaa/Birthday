99815a0e5a25eae3487a870710dce3e4745772ba
whtvrkaa <whtvrka@gmail.com>
2021-03-12 22:39:18 +0000

    commit 2

--- src/data.js
+++ src/data.js
@@ -1,37 +1,37 @@
 export default [
   {
     id: 1,
-    name: 'Bertie Yates',
-    age: 29,
+    name: 'Pablo Conde',
+    age: 26,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      'https://i.ibb.co/kDqX8Yn/me.png',
   },
   {
     id: 2,
-    name: 'Hester Hogan',
-    age: 32,
+    name: 'Nahomi Viscanzo',
+    age: 23,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+      'https://scontent.flhr2-1.fna.fbcdn.net/v/t1.0-9/156800725_10220420432325886_3879346141749638470_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=z8igVvyH3o4AX_MqLO3&_nc_ht=scontent.flhr2-1.fna&oh=0778aad2aec2f4a6d78d5f8776416598&oe=60720CBB',
   },
   {
     id: 3,
-    name: 'Larry Little',
-    age: 36,
+    name: 'Andrius Raudeliunas',
+    age: 32,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      'https://scontent.flhr2-1.fna.fbcdn.net/v/t1.0-9/10250065_10153870557999686_3576585858755702144_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=174925&_nc_ohc=lkH7OYAORzUAX8J69h1&_nc_ht=scontent.flhr2-1.fna&oh=dbfc8b33e998997fd306b3540b397876&oe=607059FD',
   },
   {
     id: 4,
-    name: 'Sean Walsh',
-    age: 34,
+    name: 'Chiara Cellere',
+    age: 28,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      'https://scontent.flhr2-2.fna.fbcdn.net/v/t1.0-9/13716005_10208546842262875_5340233013263441195_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=174925&_nc_ohc=nKSLNrVhrTsAX8Pe-3A&_nc_ht=scontent.flhr2-2.fna&oh=26a51d59b44be7c6e7e31b827a6994ec&oe=6070590D',
   },
   {
     id: 5,
-    name: 'Lola Gardner',
+    name: 'Klaudia Krzywda',
     age: 29,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      'https://scontent.flhr2-1.fna.fbcdn.net/v/t1.0-9/18199018_1401355119910909_6280140363231221759_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=h1sZygGeUVIAX9J49cA&_nc_ht=scontent.flhr2-1.fna&oh=1f4139ec77f715c67b9485fbdbef852c&oe=6070A44E',
   },
 ];
